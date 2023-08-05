12505f9cf1d5252c2abf5fdd61be47f4c71453c3
willrhm <raulwilliamhuachacamejia@gmail.com>
2023-08-05 15:31:05 -0500

    Implementing AdcInit function in Adc library

--- Adc.c
+++ Adc.c
@@ -4,7 +4,22 @@
 
 void AdcInit()
 {
-    
+    /*PIN A0 AS A ANALOGIC*/
+    ADCON1bits.PCFG0 = 0;
+    ADCON1bits.PCFG1 = 1;
+    ADCON1bits.PCFG2 = 1;
+    ADCON1bits.PCFG3 = 1;
+    /*REFERENCE*/
+    ADCON1bits.VCFG0 = 0;   //VDD
+    ADCON1bits.VCFG1 = 0;   //VSS
+    /*UNABLE EL ADC AND CHOSE CHANNEL 0*/
+    ADCON0 = 0;
+    /*FOSC = 6*/
+    ADCON2bits.ADCS = 1;
+    /*TAD = 4*/
+    ADCON2bits.ACQT = 2;
+    /*JUSTIFICATION RIGHT*/
+    ADCON2bits.ADFM = 1;
 }
 
 unsigned int AdcRead(unsigned char ch)
